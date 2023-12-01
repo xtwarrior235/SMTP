580ecc3f91b87b0013d87bdc896543df8d05831f
xtwarrior235 <93034470+xtwarrior235@users.noreply.github.com>
2023-12-01 09:38:59 +0800

    Add files via upload

--- smtp_game.py
+++ smtp_game.py
@@ -6,11 +6,14 @@ settings_filename = 'client_settings.txt'
 
 smtpcommand = 'HELO'
 mode = 'command'
+email = []
 def reset():
     global smtpcommand
     global mode
+    global email
     smtpcommand = 'HELO'
     mode = 'command'
+    email = []
 
 
 terminal = 'game'
@@ -32,48 +35,83 @@ if terminal_api.connect(terminal):
     terminal_api.prompt = f"[{team_colour}]SMTP[/{team_colour}]@[{team_colour}]{terminal_api.game_ip_address}[/{team_colour}]>"
 
     message = ''
-    email = ("")
+
+    
+
+
+
 
     while message != 'quit':
+
+
+        console.print('''
+        __________________________
+        |\                      /|
+        | \        ASCII       / |
+        |  \  (/◕ヮ◕)/ (◠‿◠)  /  | 
+        |  /\________________/\  | 
+        | /                    \ | 
+        |/                      \|
+        |________________________| 
+
+                        ''')
+
+
         message = console.input(terminal_api.prompt)
         if message == 'HELO':
             if smtpcommand == 'HELO':
                 mode = 'email'
-                smtpcommand = 'MAIL FROM':
-                email.append(message)
+                smtpcommand = 'DIR'
+                
             else:
                 print("Syntax Error")
                 reset()
 
-        elif message.startswith('MAIL FROM'):
-            if smtpcommand == 'MAIL FROM':
-                mode = 'email'
-                smtpcommand = 'RCPT TO'
-                email.append(message)
-            else:
-                print("Syntax Error")
-                reset()
 
-        elif message.startswith('RCPT TO'):
-            if smtpcommand == 'RCPT TO':
-                mode = 'email'
-                smtpcommand = 'DATA'
-                email.append(message)
-            else:
-                print("Syntax Error")
-                reset()
+        elif mode == 'email':
+            if message.startswith('DIRECTIVE:'):
+                if smtpcommand == 'DIR':
+                    smtpcommand = 'MAIL FROM'
+                    email.append(message)
+                    
+                else:
+                    print("Syntax Error")
+                    reset()
+            elif message.startswith('MAIL FROM:'):
+                if smtpcommand == 'MAIL FROM':
+                    smtpcommand = 'RCPT TO'
+                    email.append(message)
+                    
+                else:
+                    print("Syntax Error")
+                    reset()
 
-        elif smtpcommand == 'DATA':
-            message = input('>')
+            elif message.startswith('RCPT TO:'):
+                if smtpcommand == 'RCPT TO':
+                    smtpcommand = 'DATA'
+                    email.append(message)
+                    
+                else:
+                    print("Syntax Error")
+                    reset()
+
+            elif message.startswith('DATA'):
+                if message.endswith('.'):
+                    email.append(message)
+                    terminal_api.send(email)
+                else:
+                    print("Syntax Error")
+                    reset()
+            else:
+                print('Syntax Error')
+                reset()
 
             
-                
-
-
-
         elif message.startswith('rem:'):
+            reset()
             terminal_api.remove_item(message[4:])
         else:
+            reset()
             terminal_api.send(message)
         time.sleep(0.2)
     terminal_api.disconnect()
